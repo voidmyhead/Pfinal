7928213461f020e157459390d0416128ccc9e60b
voidmyhead <48941941+voidmyhead@users.noreply.github.com>
2021-02-13 00:18:56 +0900

    Add files via upload
    해당 프로젝트의 xaml코드들과 cs 코드들을 업로드

---
+++ Chart.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Data.SqlClient;
+
+
+namespace Pfinal
+{
+    /// <summary>
+    /// Chart.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Chart : Window
+    {
+        public Chart()
+        {
+            InitializeComponent();
+
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "server=.\\SQLEXPRESS; database = student; user id = sa; pwd = 412563;";
+            Conn.Open();
+            SqlCommand Command = new SqlCommand("SELECT * FROM tblStudent", Conn);
+            SqlDataReader Reader = Command.ExecuteReader();
+
+            List<KeyValuePair<string, int>> valueList = new List<KeyValuePair<string, int>>();
+
+            while (Reader.Read()) {
+                string[] array1 = new string[] { Reader[0].ToString(), Reader[4].ToString() };
+                
+                valueList.Add(new KeyValuePair<string, int>(array1[0], int.Parse(array1[1])));
+            }
+
+            xColumnChart.DataContext = valueList;
+
+            Reader.Close();
+            Conn.Close();
+
+        }
+
+    }
+}
---
+++ Print_Score.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Data.SqlClient;
+
+namespace Pfinal
+{
+
+    public partial class Print_Score : Window
+    {
+        public Print_Score()
+        {
+            InitializeComponent();
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "server=.\\SQLEXPRESS; database = student; user id = sa; pwd = 412563;";
+            Conn.Open();
+            SqlCommand Command = new SqlCommand("SELECT * FROM tblStudent", Conn);
+            SqlDataReader Reader = Command.ExecuteReader();
+            string strRecord;
+            while (Reader.Read())
+            { 
+                strRecord = string.Format("이름 : {0} 영어 : {1} 수학 : {2} 평균 : {3} 총점 : {4}", Reader[0], Reader[1], Reader[2], Reader[3], Reader[4]);
+                Score_Viewer.Items.Add(strRecord);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) // 새로고침
+        {
+            Score_Viewer.Items.Clear();
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "server=.\\SQLEXPRESS; database = student; user id = sa; pwd = 412563;";
+            Conn.Open();
+            SqlCommand Command = new SqlCommand("SELECT * FROM tblStudent", Conn);
+            SqlDataReader Reader = Command.ExecuteReader();
+            string strRecord;
+            while (Reader.Read())
+            {
+                strRecord = string.Format("이름 : {0} 영어 : {1} 수학 : {2} 평균 : {3} 총점 : {4}", Reader[0], Reader[1], Reader[2], Reader[3], Reader[4]);
+                Score_Viewer.Items.Add(strRecord);
+            }
+
+            Reader.Close();
+            Conn.Close();
+        }
+    }
+}
