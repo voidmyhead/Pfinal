6ca3f03dcc27459d064d754c75aaee4f2363496f
voidmyhead <48941941+voidmyhead@users.noreply.github.com>
2021-02-15 00:33:24 +0900

    Update Print_Score.xaml.cs
    버튼이 2개가 되면서 각각의 버튼이름을 새로 지어주고
    정렬 버튼이 동작하도록 버튼 클릭 이벤트 코드를 추가

--- Print_Score.xaml.cs
+++ Print_Score.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+using System.Windows;
 using System.Data.SqlClient;
 
 namespace Pfinal
@@ -16,13 +16,16 @@ namespace Pfinal
             SqlDataReader Reader = Command.ExecuteReader();
             string strRecord;
             while (Reader.Read())
-            { 
+            {
+                //strRecord를 해당 포맷으로 출력
                 strRecord = string.Format("이름 : {0} 영어 : {1} 수학 : {2} 평균 : {3} 총점 : {4}", Reader[0], Reader[1], Reader[2], Reader[3], Reader[4]);
                 Score_Viewer.Items.Add(strRecord);
             }
+            Reader.Close();
+            Conn.Close();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e) // 새로고침
+        private void Re_Button_Click(object sender, RoutedEventArgs e) // 새로고침 버튼
         {
             Score_Viewer.Items.Clear();
             SqlConnection Conn = new SqlConnection();
@@ -33,6 +36,27 @@ namespace Pfinal
             string strRecord;
             while (Reader.Read())
             {
+                //지정된 형식에 맞는 문자열로 변환
+                strRecord = string.Format("이름 : {0} 영어 : {1} 수학 : {2} 평균 : {3} 총점 : {4}", Reader[0], Reader[1], Reader[2], Reader[3], Reader[4]);
+                Score_Viewer.Items.Add(strRecord);
+            }
+
+            Reader.Close();
+            Conn.Close();
+        }
+
+        private void S_Button_Click(object sender, RoutedEventArgs e)//성적순 정렬
+        {
+            Score_Viewer.Items.Clear();
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "server=.\\SQLEXPRESS; database = student; user id = sa; pwd = 412563;";
+            Conn.Open();
+            SqlCommand Command = new SqlCommand("SELECT * FROM tblStudent ORDER BY Sum DESC", Conn); //성적을 내림차순 정렬
+            SqlDataReader Reader = Command.ExecuteReader();
+            string strRecord;
+            while (Reader.Read())
+            {
+                //지정된 형식에 맞는 문자열로 변환
                 strRecord = string.Format("이름 : {0} 영어 : {1} 수학 : {2} 평균 : {3} 총점 : {4}", Reader[0], Reader[1], Reader[2], Reader[3], Reader[4]);
                 Score_Viewer.Items.Add(strRecord);
             }
